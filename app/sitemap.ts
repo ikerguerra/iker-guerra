a87b83eed85c451f4a381a051e37c274439e4810
ikerguerra <152733086+ikerguerra@users.noreply.github.com>
2026-03-19 20:08:44 +0100

    feat: add sitemap with main application routes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,22 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://ikerguerra.vercel.app',
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/proyectos`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/contacto`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
         },
     ]
 }
